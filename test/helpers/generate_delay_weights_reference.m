b09102facd27f5d56792c2d17ad391540e8203cc
mrymaltin <58702246+mrymaltin@users.noreply.github.com>
2026-03-09 16:36:09 -0400

    Update generate_delay_weights_reference function

--- test/helpers/generate_delay_weights_reference.m
+++ test/helpers/generate_delay_weights_reference.m
@@ -1,3 +1,9 @@
-function generate_delay_weights_reference
-    % Your implementation here
+function w = generate_delay_weights_reference(N, delay_min)
+    DT_MIN = 5;
+    dt_elapsed = (N:-1:1)' * DT_MIN;
+    window = delay_min / 2;
+    w = exp(-((dt_elapsed - delay_min).^2) / window^2);
+    if sum(w) > 1e-9
+        w = w / sum(w);
+    end
 end
